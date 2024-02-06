564f64f1e391d8aedf9fa58a58b7b79d90327026
MelekErisgin34 <147922709+MelekErisgin34@users.noreply.github.com>
2024-02-06 10:52:39 +0300

    Add files via upload

---
+++ sıra alma queue/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace sıra_alma_queue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGise3 = new System.Windows.Forms.Label();
+            this.lblGise2 = new System.Windows.Forms.Label();
+            this.lblGise1 = new System.Windows.Forms.Label();
+            this.lblSiraAl = new System.Windows.Forms.Label();
+            this.btnSiraAl = new System.Windows.Forms.Button();
+            this.btnGise3 = new System.Windows.Forms.Button();
+            this.btnGise2 = new System.Windows.Forms.Button();
+            this.btnGise1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGise3
+            // 
+            this.lblGise3.AutoSize = true;
+            this.lblGise3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGise3.Location = new System.Drawing.Point(682, 130);
+            this.lblGise3.Name = "lblGise3";
+            this.lblGise3.Size = new System.Drawing.Size(35, 37);
+            this.lblGise3.TabIndex = 15;
+            this.lblGise3.Text = "0";
+            // 
+            // lblGise2
+            // 
+            this.lblGise2.AutoSize = true;
+            this.lblGise2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGise2.Location = new System.Drawing.Point(385, 130);
+            this.lblGise2.Name = "lblGise2";
+            this.lblGise2.Size = new System.Drawing.Size(35, 37);
+            this.lblGise2.TabIndex = 14;
+            this.lblGise2.Text = "0";
+            // 
+            // lblGise1
+            // 
+            this.lblGise1.AutoSize = true;
+            this.lblGise1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGise1.Location = new System.Drawing.Point(93, 130);
+            this.lblGise1.Name = "lblGise1";
+            this.lblGise1.Size = new System.Drawing.Size(35, 37);
+            this.lblGise1.TabIndex = 13;
+            this.lblGise1.Text = "0";
+            // 
+            // lblSiraAl
+            // 
+            this.lblSiraAl.AutoSize = true;
+            this.lblSiraAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSiraAl.Location = new System.Drawing.Point(405, 254);
+            this.lblSiraAl.Name = "lblSiraAl";
+            this.lblSiraAl.Size = new System.Drawing.Size(35, 37);
+            this.lblSiraAl.TabIndex = 12;
+            this.lblSiraAl.Text = "0";
+            // 
+            // btnSiraAl
+            // 
+            this.btnSiraAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSiraAl.Location = new System.Drawing.Point(304, 294);
+            this.btnSiraAl.Name = "btnSiraAl";
+            this.btnSiraAl.Size = new System.Drawing.Size(234, 125);
+            this.btnSiraAl.TabIndex = 11;
+            this.btnSiraAl.Text = "SIRA AL";
+            this.btnSiraAl.UseVisualStyleBackColor = true;
+            this.btnSiraAl.Click += new System.EventHandler(this.btnSiraAl_Click);
+            // 
+            // btnGise3
+            // 
+            this.btnGise3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGise3.Location = new System.Drawing.Point(618, 31);
+            this.btnGise3.Name = "btnGise3";
+            this.btnGise3.Size = new System.Drawing.Size(152, 76);
+            this.btnGise3.TabIndex = 10;
+            this.btnGise3.Text = "GİŞE 3";
+            this.btnGise3.UseVisualStyleBackColor = true;
+            this.btnGise3.Click += new System.EventHandler(this.btnGise3_Click);
+            // 
+            // btnGise2
+            // 
+            this.btnGise2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGise2.Location = new System.Drawing.Point(323, 31);
+            this.btnGise2.Name = "btnGise2";
+            this.btnGise2.Size = new System.Drawing.Size(152, 76);
+            this.btnGise2.TabIndex = 9;
+            this.btnGise2.Text = "GİŞE 2";
+            this.btnGise2.UseVisualStyleBackColor = true;
+            this.btnGise2.Click += new System.EventHandler(this.btnGise2_Click);
+            // 
+            // btnGise1
+            // 
+            this.btnGise1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGise1.Location = new System.Drawing.Point(30, 31);
+            this.btnGise1.Name = "btnGise1";
+            this.btnGise1.Size = new System.Drawing.Size(152, 76);
+            this.btnGise1.TabIndex = 8;
+            this.btnGise1.Text = "GİŞE 1";
+            this.btnGise1.UseVisualStyleBackColor = true;
+            this.btnGise1.Click += new System.EventHandler(this.btnGise1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGise3);
+            this.Controls.Add(this.lblGise2);
+            this.Controls.Add(this.lblGise1);
+            this.Controls.Add(this.lblSiraAl);
+            this.Controls.Add(this.btnSiraAl);
+            this.Controls.Add(this.btnGise3);
+            this.Controls.Add(this.btnGise2);
+            this.Controls.Add(this.btnGise1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGise3;
+        private System.Windows.Forms.Label lblGise2;
+        private System.Windows.Forms.Label lblGise1;
+        private System.Windows.Forms.Label lblSiraAl;
+        private System.Windows.Forms.Button btnSiraAl;
+        private System.Windows.Forms.Button btnGise3;
+        private System.Windows.Forms.Button btnGise2;
+        private System.Windows.Forms.Button btnGise1;
+    }
+}
+
---
+++ sıra alma queue/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sıra_alma_queue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Queue<int> queue = new Queue<int>(); //Tüm butonlardan erişilebilmesi için Global tanımlama yaptık.
+
+        int sayac = 1;
+
+        private void btnSiraAl_Click(object sender, EventArgs e)
+        {
+
+            queue.Enqueue(sayac); 
+            lblSiraAl.Text = sayac.ToString(); 
+            sayac++;
+
+
+        }
+
+        private void btnGise1_Click(object sender, EventArgs e)
+        {
+
+            lblGise1.Text = SiraCagir();
+        }
+
+        private void btnGise2_Click(object sender, EventArgs e)
+        {
+            lblGise2.Text = SiraCagir();
+        }
+
+        private void btnGise3_Click(object sender, EventArgs e)
+        {
+            lblGise3.Text = SiraCagir();
+
+            private string SiraCagir()
+            {
+                if (queue.Count > 0)
+                {
+                    string sira = queue.Dequeue().ToString(); //Kuyruktan önce girilen değeri kaldırdık.
+                    return sira;
+                }
+                else
+                {
+                    return "Musteri yok..";
+                }
+
+            }
+
+        }
+    }
+}
